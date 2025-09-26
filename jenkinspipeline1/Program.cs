5b4e2c306b97938a0fab968912808a50027f1a39
Riyaanshh <riyaansh.jagtap1665@gmail.com>
2025-09-26 17:59:01 +0530

    Add project files.

---
+++ jenkinspipeline1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jenkinspipeline1
+{
+    public class Program
+    {
+        public void Add()
+        {
+            Console.WriteLine("hello world");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
